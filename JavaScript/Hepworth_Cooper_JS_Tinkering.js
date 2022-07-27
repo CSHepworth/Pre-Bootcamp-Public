eb47281c0418d90403054974d91bb294451b072e
Supaweapon39 <hep15006@byui.edu>
2022-07-26 22:27:30 -0600

    tinkering

---
+++ JavaScript/Hepworth_Cooper_JS_Tinkering.js
@@ -0,0 +1,12 @@
+var childHeight = 57;
+console.log(childHeight);
+function displayIfChildIsAbleToRideTheRollerCoaster() {
+    if (childHeight > 52) {
+        console.log("Get on that ride, kiddo!");
+    }
+    else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+}
+
+displayIfChildIsAbleToRideTheRollerCoaster();
